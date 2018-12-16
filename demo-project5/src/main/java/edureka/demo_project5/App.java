2e6bac1c82cf510c09b6afadb7ff9c0a04771c92
edureka <edureka@master>
2018-12-16 07:40:22 +0530

    testing

---
+++ demo-project5/src/main/java/edureka/demo_project5/App.java
@@ -0,0 +1,13 @@
+package edureka.demo_project5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
